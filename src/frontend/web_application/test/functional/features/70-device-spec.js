6bc2f778cea4fa0740fec2cfcf3369141d719f2c
David Epely <esion99@gmail.com>
2018-01-24 18:12:11 +0100

    add functional tests for new device redirection

---
+++ src/frontend/web_application/test/functional/features/70-device-spec.js
@@ -0,0 +1,18 @@
+const { signin } = require('../utils/user-util');
+
+describe('device', () => {
+  const clearKeypairInLocalStorage = () => browser.get('/')
+    .then(() => browser.pause())
+    .then(() => browser.executeScript(`
+      window.localStorage.removeItem('device.curve');
+      window.localStorage.removeItem('device.hash');
+      window.localStorage.removeItem('device.id');
+      window.localStorage.removeItem('device.priv');
+    `));
+
+  it('redirect to device management on a new device for an existing account', () => {
+    clearKeypairInLocalStorage()
+      .then(() => signin())
+      .then(() => expect(browser.getCurrentUrl()).toContain('/settings/devices/'));
+  });
+});
